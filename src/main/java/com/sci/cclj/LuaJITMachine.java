1bd9b2234a7d5377340a86770eacfca3823031c6
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-01-11 23:35:19 -0500

    Slightly better loading of native library

--- src/main/java/com/sci/cclj/LuaJITMachine.java
+++ src/main/java/com/sci/cclj/LuaJITMachine.java
@@ -1,8 +1,30 @@
 package com.sci.cclj;
 
+import java.io.File;
+import java.net.URISyntaxException;
+
 public final class LuaJITMachine {
     static {
-        System.load("/home/sci4me/Projects/CCLuaJIT/build/natives/cclj.so");
+        try {
+            final String libraryExtension;
+
+            final String os = System.getProperty("os.name").toLowerCase();
+            if(os.contains("win")) {
+                throw new RuntimeException("Windows is currently not supported");
+            } else if(os.contains("mac")) {
+                throw new RuntimeException("OSX is currently not supported");
+            } else if(os.contains("nux") || os.contains("nix") || os.contains("aix")) {
+                libraryExtension = "so";
+            } else {
+                throw new RuntimeException("Unknown operating system: '" + os + "'");
+            }
+
+            final File modsDirectory = new File(LuaJITMachine.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getParentFile();
+            final File library = new File(modsDirectory, "cclj." + libraryExtension);
+            System.load(library.getPath());
+        } catch(final URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
     public LuaJITMachine() {
