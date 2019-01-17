dbfc0ccd9d0dd598f5834f86c003981865a8a57e
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-01-17 11:29:04 -0500

    Begin work on the task scheduler

--- src/main/java/com/sci/cclj/computer/TaskScheduler.java
+++ src/main/java/com/sci/cclj/computer/TaskScheduler.java
@@ -1,161 +1,98 @@
 package com.sci.cclj.computer;
 
+import com.sci.cclj.computer.util.ReferencingCache;
 import dan200.computercraft.core.computer.Computer;
 import dan200.computercraft.core.computer.ITask;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.WeakHashMap;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public final class TaskScheduler {
     public static final TaskScheduler INSTANCE = new TaskScheduler();
 
-    private final Object mainLock = new Object();
-    private final Object defaultQueue = new Object();
+    private final Object lock = new Object();
+    private final Object defaultOwner = new Object();
 
     private boolean running;
-    private boolean stopped;
-    private Thread mainThread;
 
-    private final WeakHashMap<Object, LinkedBlockingQueue<ITask>> computerTasks;
-    private final List<LinkedBlockingQueue<ITask>> computerTasksActive;
-    private final List<LinkedBlockingQueue<ITask>> computerTasksPending;
-    private final Object monitor;
+    private final ReferencingCache<TaskExecutor> taskExecutorCache;
+
+    private final WeakHashMap<Object, BlockingQueue<ITask>> computerTaskQueues;
+    private final BlockingQueue<BlockingQueue<ITask>> computerTaskQueuesActive;
+    private final Set<BlockingQueue<ITask>> computerTaskQueuesActiveSet;
+
+    private final BlockingQueue<TaskExecutor> blockedTaskExecutors;
 
     private TaskScheduler() {
-        this.computerTasks = new WeakHashMap<>();
-        this.computerTasksActive = new ArrayList<>();
-        this.computerTasksPending = new ArrayList<>();
-        this.monitor = new Object();
+        this.taskExecutorCache = new ReferencingCache<>(TaskExecutor::new);
+
+        this.computerTaskQueues = new WeakHashMap<>();
+        this.computerTaskQueuesActive = new LinkedBlockingQueue<>();
+        this.computerTaskQueuesActiveSet = new HashSet<>();
+
+        this.blockedTaskExecutors = new LinkedBlockingQueue<>();
     }
 
     public void start() {
-        synchronized(this.mainLock) {
-            if(this.running) {
-                this.stopped = false;
-                return;
-            }
-
-            this.mainThread = new Thread(this::run, "CCLJ-TaskScheduler-Main");
-            this.mainThread.setDaemon(true);
-            this.mainThread.start();
-
+        synchronized(this.lock) {
+            if(this.running) return;
             this.running = true;
         }
+
+        final Thread dispatcherThread = new Thread(this::run, "CCLJ-TaskScheduler-Dispatcher");
+        dispatcherThread.setDaemon(true);
+        dispatcherThread.start();
     }
 
     public void stop() {
-        synchronized(this.mainLock) {
-            if(this.running) {
-                this.stopped = true;
-                this.mainThread.interrupt();
-            }
-        }
+        throw new RuntimeException("Unexpected call to TaskScheduler::stop");
     }
 
     public void queueTask(final ITask task, final Computer computer) {
         Object queueObject = computer;
-        if(computer == null) queueObject = this.defaultQueue;
+        if(computer == null) queueObject = this.defaultOwner;
 
-        LinkedBlockingQueue<ITask> queue = this.computerTasks.get(queueObject);
-        if(queue == null) {
-            queue = new LinkedBlockingQueue<>();
-            this.computerTasks.put(queueObject, queue);
-        }
-
-        synchronized(this.computerTasksPending) {
-            if(queue.offer(task)) {
-                if(!this.computerTasksPending.contains(queue)) {
-                    this.computerTasksPending.add(queue);
-                }
-            } else {
-                throw new RuntimeException("Event queue overflow");
+        BlockingQueue<ITask> queue;
+        synchronized(this.computerTaskQueues) {
+            queue = this.computerTaskQueues.get(queueObject);
+            if(queue == null) {
+                queue = new LinkedBlockingQueue<>();
+                this.computerTaskQueues.put(queueObject, queue);
             }
         }
 
-        synchronized(this.monitor) {
-            this.monitor.notify();
+        synchronized(this.lock) {
+            if(queue.offer(task) && !this.computerTaskQueuesActiveSet.contains(queue)) {
+                this.computerTaskQueuesActive.add(queue);
+                this.computerTaskQueuesActiveSet.add(queue);
+            }
         }
     }
 
     private void run() {
-        while(true) {
-            synchronized(this.computerTasksPending) {
-                if(!this.computerTasksPending.isEmpty()) {
-                    final Iterator<LinkedBlockingQueue<ITask>> it = this.computerTasksPending.iterator();
-                    while(it.hasNext()) {
-                        final LinkedBlockingQueue<ITask> queue = it.next();
 
-                        if(!this.computerTasksActive.contains(queue)) {
-                            this.computerTasksActive.add(queue);
-                        }
+    }
 
-                        it.remove();
-                    }
-                }
-            }
+    private static final class TaskExecutor implements Runnable {
+        private final TaskRunner runner;
 
-            final Iterator<LinkedBlockingQueue<ITask>> it = this.computerTasksActive.iterator();
-            while(it.hasNext()) {
-                final LinkedBlockingQueue<ITask> queue = it.next();
+        TaskExecutor() {
+            this.runner = new TaskRunner();
+        }
 
-                if(queue == null || queue.isEmpty()) {
-                    continue;
-                }
+        @Override
+        public void run() {
 
-                synchronized(this.mainLock) {
-                    if(this.stopped) {
-                        this.running = false;
-                        this.mainThread = null;
-                    }
-                }
+        }
+    }
 
-                try {
-                    final ITask task = queue.take();
+    private static final class TaskRunner implements Runnable {
+        @Override
+        public void run() {
 
-                    final Thread worker = new Thread(task::execute, "CCLJ-TaskScheduler-Worker");
-                    worker.setDaemon(true);
-                    worker.start();
-                    worker.join(7000);
-
-                    if(worker.isAlive()) {
-                        final Computer computer = task.getOwner();
-
-                        if(computer != null) {
-                            computer.abort(false);
-                            worker.join(1500);
-
-                            if(worker.isAlive()) {
-                                computer.abort(true);
-                                worker.join(1500);
-                            }
-                        }
-
-                        if(worker.isAlive()) {
-                            worker.interrupt();
-                        }
-                    }
-                } catch(final InterruptedException e) {
-                    continue;
-                }
-
-                synchronized(queue) {
-                    if(queue.isEmpty()) {
-                        it.remove();
-                    }
-                }
-            }
-
-            while(this.computerTasksActive.isEmpty() && this.computerTasksPending.isEmpty()) {
-                synchronized(this.monitor) {
-                    try {
-                        this.monitor.wait();
-                    } catch(final InterruptedException ignored) {
-                    }
-                }
-            }
         }
     }
 }
