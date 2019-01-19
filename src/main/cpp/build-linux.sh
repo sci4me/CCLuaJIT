630870e9421be634feabf4c14bfa823f0bd48ee8
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-01-18 21:08:30 -0500

    Fix output path for linux build

--- src/main/cpp/build-linux.sh
+++ src/main/cpp/build-linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ -shared -fPIC -Wl,-rpath,'$ORIGIN' -o cclj.so -I"${JAVA_HOME}/include" -I"$JAVA_HOME/include/linux" -I"LuaJIT/include" -L"$(realpath LuaJIT/bin/linux)" -l:libluajit-5.1.so.2 LuaJITMachine.cpp
+g++ -shared -fPIC -Wl,-rpath,'$ORIGIN' -o out/cclj.so -I"${JAVA_HOME}/include" -I"$JAVA_HOME/include/linux" -I"LuaJIT/include" -L"$(realpath LuaJIT/bin/linux)" -l:libluajit-5.1.so.2 LuaJITMachine.cpp
