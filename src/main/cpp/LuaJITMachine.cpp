78f2c391b1d780266fd371cdf8e86159fb1d00cb
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-01-19 19:52:00 -0500

    Get rid of potential refactors comment in cpp code

--- src/main/cpp/LuaJITMachine.cpp
+++ src/main/cpp/LuaJITMachine.cpp
@@ -35,11 +35,9 @@ extern "C" {
 #include <lauxlib.h>
 }
 
-/*
-    Potential refactors:
-        - change "obj" to "self"
-        - for functions that take JNIEnv* and jobject as the first parameter, use a macro to do that in the preprocessor
-*/
+//
+// Helper Macros
+//
 
 #define CCLJ_JNIVERSION JNI_VERSION_1_6
 #define CCLJ_JNIEXPORT(rtype, name, ...) JNIEXPORT rtype JNICALL Java_com_sci_cclj_computer_LuaJITMachine_##name(JNIEnv *env, jobject obj, ##__VA_ARGS__)
