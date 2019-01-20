fcea0a232b2ead5a713f79f92ba6da258d7dfe0a
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-01-20 17:29:03 -0500

    Work on things; got OSX compiling, not sure if it's correct though

--- build-natives.sh
+++ build-natives.sh
@@ -6,14 +6,31 @@ docker run --name $CONTAINER_NAME cclj_build /root/build.sh
 
 CONTAINER_ID=$(docker ps -aqf "name=$CONTAINER_NAME")
 
-docker cp $CONTAINER_ID:/root/out/cclj.so build/libs/cclj.so
-docker cp $CONTAINER_ID:/root/out/cclj.dll build/libs/cclj.dll
-docker cp $CONTAINER_ID:/root/out/cclj.dylib build/libs/cclj.dylib
+status=1
 
-docker cp $CONTAINER_ID:/root/LuaJIT/bin/linux/libluajit-5.1.so.2 build/libs
-docker cp $CONTAINER_ID:/root/LuaJIT/bin/windows/lua51.dll build/libs
-docker cp $CONTAINER_ID:/root/LuaJIT/bin/osx/libluajit-5.1.2.dylib build/libs
+docker cp ${CONTAINER_ID}:/root/out/cclj.so build/libs/cclj.so
+if [[ $? -ne 0 ]]; then
+    status=0
+fi
 
-docker container rm $CONTAINER_ID
+docker cp ${CONTAINER_ID}:/root/out/cclj.dll build/libs/cclj.dll
+if [[ $? -ne 0 ]]; then
+    status=0
+fi
 
-# @TODO: fail build if one of the libs didn't compile
+docker cp ${CONTAINER_ID}:/root/out/cclj.dylib build/libs/cclj.dylib
+if [[ $? -ne 0 ]]; then
+    status=0
+fi
+
+docker cp ${CONTAINER_ID}:/root/LuaJIT/bin/linux/libluajit-5.1.so.2 build/libs
+docker cp ${CONTAINER_ID}:/root/LuaJIT/bin/windows/lua51.dll build/libs
+docker cp ${CONTAINER_ID}:/root/LuaJIT/bin/osx/libluajit-5.1.2.dylib build/libs
+
+docker container rm ${CONTAINER_ID}
+
+if [[ $status -ne 1 ]]; then
+    echo "Building natives failed!"
+fi
+
+exit $status
